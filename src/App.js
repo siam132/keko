e1191ec39fd92507eaf8e916072056df45568332
Shahidullah <shahidullah.rahman223@gmail.com>
2020-02-06 23:44:54 -0500

    added bootstrap npm package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import MainPage from './pages/HomePage'
+import bootstrap  from 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
+    <div>
    <MainPage/>
+   </div>
   );
 }
 
