208f69356847a1e774044b05f47ad3c0c7990a31
Shahidullah <shahidullah.rahman223@gmail.com>
2020-02-06 22:48:46 -0500

    Updated file structres and removed Default Page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <header className="App-header">
+            
+          </header>
+        </div>
+      </div>
+    );
+  }
+}
